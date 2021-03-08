9a4a88c4fd43e3ddd562e1454c9d3eb40bd56863
Filippos Christianos <f.christianos@ed.ac.uk>
2021-03-08 15:26:50 +0000

    Refactors: Cleans up configs

--- blazingma/utils/loggers.py
+++ blazingma/utils/loggers.py
@@ -3,37 +3,24 @@ import logging
 from hashlib import sha256
 from typing import Dict
 
-import wandb
 from hydra.conf import HydraConf
 from omegaconf import DictConfig, OmegaConf
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="(%(process)d) [%(levelname).1s] - (%(asctime)s) - %(name)s >> %(message)s",
-    datefmt="%m/%d %H:%M:%S",
-)
 
 
 class Logger:
     def __init__(self, project_name, cfg: DictConfig) -> None:
-        config_hash = sha256(
+        self.config_hash = sha256(
             json.dumps(
                 {k: v for k, v in OmegaConf.to_container(cfg).items() if k != "seed"},
                 sort_keys=True,
             ).encode("utf8")
         ).hexdigest()[-10:]
-        self._run = wandb.init(
-            project=project_name,
-            config=OmegaConf.to_container(cfg),
-            monitor_gym=True,
-            group=config_hash,
-        )
 
     def log_metrics(self, d: Dict):
-        self._run.log(d)
+        ...
 
     def watch(self, model):
-        self._run.watch(model)
+        ...
 
     def debug(self, *args, **kwargs):
         return logging.debug(*args, **kwargs)
@@ -49,3 +36,22 @@ class Logger:
 
     def critical(self, *args, **kwargs):
         return logging.critical(*args, **kwargs)
+
+
+class WandbLogger(Logger):
+    def __init__(self, project_name, cfg: DictConfig) -> None:
+        import wandb
+
+        super().__init__(project_name, cfg)
+        self._run = wandb.init(
+            project=project_name,
+            config=OmegaConf.to_container(cfg),
+            monitor_gym=True,
+            group=self.config_hash,
+        )
+
+    def log_metrics(self, d: Dict):
+        self._run.log(d)
+
+    def watch(self, model):
+        self._run.watch(model)
