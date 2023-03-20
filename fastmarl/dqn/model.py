8099ad0c66bfd77829032457546749c66e4933d4
Filippos Christianos <f.christianos@ed.ac.uk>
2023-03-20 15:50:27 +0000

    Feature: qmix implementation

--- fastmarl/dqn/model.py
+++ fastmarl/dqn/model.py
@@ -9,10 +9,18 @@ import torch.nn.functional as F
 from gym.spaces import flatdim
 from fastmarl.utils.models import MultiAgentSEPSNetwork, MultiAgentFCNetwork
 from fastmarl.utils.standarize_stream import RunningMeanStd
+from einops import rearrange, repeat
+
 
 class QNetwork(nn.Module):
     def __init__(
-        self, obs_space, action_space, cfg, layers, critic, device,
+        self,
+        obs_space,
+        action_space,
+        cfg,
+        layers,
+        critic,
+        device,
     ):
         super().__init__()
         hidden_size = list(layers)
@@ -34,8 +42,12 @@ class QNetwork(nn.Module):
             self.critic = MultiAgentFCNetwork(obs_shape, hidden_size, action_shape)
             self.target = MultiAgentFCNetwork(obs_shape, hidden_size, action_shape)
         else:
-            self.critic = MultiAgentSEPSNetwork(obs_shape, hidden_size + [action_shape[0]], critic.parameter_sharing)
-            self.target = MultiAgentSEPSNetwork(obs_shape, hidden_size + [action_shape[0]], critic.parameter_sharing)
+            self.critic = MultiAgentSEPSNetwork(
+                obs_shape, hidden_size + [action_shape[0]], critic.parameter_sharing
+            )
+            self.target = MultiAgentSEPSNetwork(
+                obs_shape, hidden_size + [action_shape[0]], critic.parameter_sharing
+            )
 
         self.soft_update(1.0)
 
@@ -44,6 +56,7 @@ class QNetwork(nn.Module):
 
         if type(optimizer) is str:
             optimizer = getattr(optim, optimizer)
+        self.optimizer_class = optimizer
 
         self.optimizer = optimizer(self.critic.parameters(), lr=lr, eps=optim_eps)
 
@@ -53,9 +66,9 @@ class QNetwork(nn.Module):
 
         self.updates = 0
         self.target_update_interval_or_tau = cfg.target_update_interval_or_tau
-        
+
         self.standarize_returns = cfg.standarize_returns
-        self.ret_ms = RunningMeanStd(shape=(self.n_agents, ))
+        self.ret_ms = RunningMeanStd(shape=(self.n_agents,))
 
         print(self)
 
@@ -77,7 +90,9 @@ class QNetwork(nn.Module):
         obs = [batch[f"obs{i}"] for i in range(self.n_agents)]
         nobs = [batch[f"next_obs{i}"] for i in range(self.n_agents)]
         action = torch.stack([batch[f"act{i}"].long() for i in range(self.n_agents)])
-        rewards = torch.stack([batch["rew"][:, i].view(-1, 1) for i in range(self.n_agents)])
+        rewards = torch.stack(
+            [batch["rew"][:, i].view(-1, 1) for i in range(self.n_agents)]
+        )
         done = batch["done"]
 
         with torch.no_grad():
@@ -87,11 +102,13 @@ class QNetwork(nn.Module):
 
         _, a_prime = q_tp1_values.max(-1)
         target_next_states = q_next_states.gather(2, a_prime.unsqueeze(-1))
-        target_states = rewards + self.gamma * target_next_states * (1-done)
+        target_states = rewards + self.gamma * target_next_states * (1 - done)
 
         if self.standarize_returns:
             self.ret_ms.update(target_states)
-            target_states = ((target_states - self.ret_ms.mean.view(-1, 1, 1)) / torch.sqrt(self.ret_ms.var.view(-1, 1, 1)))
+            target_states = (
+                target_states - self.ret_ms.mean.view(-1, 1, 1)
+            ) / torch.sqrt(self.ret_ms.var.view(-1, 1, 1))
 
         q_states = all_q_states.gather(2, action)
 
@@ -107,7 +124,10 @@ class QNetwork(nn.Module):
         self.update_from_target()
 
     def update_from_target(self):
-        if self.target_update_interval_or_tau > 1.0 and self.updates % self.target_update_interval_or_tau == 0:
+        if (
+            self.target_update_interval_or_tau > 1.0
+            and self.updates % self.target_update_interval_or_tau == 0
+        ):
             self.soft_update(1.0)
         elif self.target_update_interval_or_tau < 1.0:
             self.soft_update(self.target_update_interval_or_tau)
@@ -120,13 +140,12 @@ class QNetwork(nn.Module):
 
 
 class VDNetwork(QNetwork):
-
     def __init__(self, obs_space, action_space, cfg, layers, critic, device):
         super().__init__(obs_space, action_space, cfg, layers, critic, device)
-        self.ret_ms = RunningMeanStd(shape=(1, ))
+        self.ret_ms = RunningMeanStd(shape=(1,))
 
     def update(self, batch):
-        
+
         obs = [batch[f"obs{i}"] for i in range(self.n_agents)]
         nobs = [batch[f"next_obs{i}"] for i in range(self.n_agents)]
         action = torch.stack([batch[f"act{i}"].long() for i in range(self.n_agents)])
@@ -140,11 +159,13 @@ class VDNetwork(QNetwork):
 
         _, a_prime = q_tp1_values.max(-1)
         target_next_states = q_next_states.gather(2, a_prime.unsqueeze(-1)).sum(0)
-        target_states = rewards + self.gamma * target_next_states * (1-done)
+        target_states = rewards + self.gamma * target_next_states * (1 - done)
 
         if self.standarize_returns:
             self.ret_ms.update(target_states)
-            target_states = ((target_states - self.ret_ms.mean.view(-1, 1)) / torch.sqrt(self.ret_ms.var.view(-1, 1)))
+            target_states = (target_states - self.ret_ms.mean.view(-1, 1)) / torch.sqrt(
+                self.ret_ms.var.view(-1, 1)
+            )
 
         q_states = all_q_states.gather(2, action).sum(0)
 
@@ -160,3 +181,135 @@ class VDNetwork(QNetwork):
         self.update_from_target()
 
 
+class QMixer(nn.Module):
+    def __init__(self, n_agents, state_dim, embed_dim, hypernet_layers, hypernet_embed):
+        super().__init__()
+
+        self.n_agents = n_agents
+        self.state_dim = state_dim
+
+        self.embed_dim = embed_dim
+        self.hypernet_layers = hypernet_layers
+        self.hypernet_embed = hypernet_embed
+
+        if hypernet_layers == 1:
+            self.hyper_w_1 = nn.Linear(self.state_dim, self.embed_dim * self.n_agents)
+            self.hyper_w_final = nn.Linear(self.state_dim, self.embed_dim)
+        elif hypernet_layers == 2:
+            hypernet_embed = self.hypernet_embed
+            self.hyper_w_1 = nn.Sequential(
+                nn.Linear(self.state_dim, hypernet_embed),
+                nn.ReLU(),
+                nn.Linear(hypernet_embed, self.embed_dim * self.n_agents),
+            )
+            self.hyper_w_final = nn.Sequential(
+                nn.Linear(self.state_dim, hypernet_embed),
+                nn.ReLU(),
+                nn.Linear(hypernet_embed, self.embed_dim),
+            )
+        elif getattr(args, "hypernet_layers", 1) > 2:
+            raise Exception("Sorry >2 hypernet layers is not implemented!")
+        else:
+            raise Exception("Error setting number of hypernet layers.")
+
+        # State dependent bias for hidden layer
+        self.hyper_b_1 = nn.Linear(self.state_dim, self.embed_dim)
+
+        # V(s) instead of a bias for the last layers
+        self.V = nn.Sequential(
+            nn.Linear(self.state_dim, self.embed_dim),
+            nn.ReLU(),
+            nn.Linear(self.embed_dim, 1),
+        )
+
+    def forward(self, agent_qs, states):
+        agent_qs = rearrange(agent_qs, "N B 1 -> B 1 N")
+        bs = agent_qs.size(0)
+        states = states.reshape(-1, self.state_dim)
+        agent_qs = agent_qs.view(-1, 1, self.n_agents)
+        # First layer
+        w1 = torch.abs(self.hyper_w_1(states))
+        b1 = self.hyper_b_1(states)
+        w1 = w1.view(-1, self.n_agents, self.embed_dim)
+        b1 = b1.view(-1, 1, self.embed_dim)
+        hidden = F.elu(torch.bmm(agent_qs, w1) + b1)
+        # Second layer
+        w_final = torch.abs(self.hyper_w_final(states))
+        w_final = w_final.view(-1, self.embed_dim, 1)
+        # State-dependent bias
+        v = self.V(states).view(-1, 1, 1)
+        # Compute final output
+        y = torch.bmm(hidden, w_final) + v
+        # Reshape and return
+        q_tot = y.view(bs, -1)
+        return q_tot
+
+
+class QMixNetwork(QNetwork):
+    def __init__(self, obs_space, action_space, cfg, layers, critic, mixing, device):
+        super().__init__(obs_space, action_space, cfg, layers, critic, device)
+        self.ret_ms = RunningMeanStd(shape=(1,))
+
+        state_dim = sum([flatdim(o) for o in obs_space])
+        self.mixer = QMixer(self.n_agents, state_dim, **mixing)
+        self.target_mixer = QMixer(self.n_agents, state_dim, **mixing)
+        self.soft_update(1.0)
+
+        for param in self.target_mixer.parameters():
+            param.requires_grad = False
+
+        self.optimizer = self.optimizer_class(
+            list(self.critic.parameters()) + list(self.mixer.parameters()),
+            lr=cfg.lr,
+            eps=cfg.optim_eps,
+        )
+        print(self)
+
+    def update(self, batch):
+
+        obs = [batch[f"obs{i}"] for i in range(self.n_agents)]
+        nobs = [batch[f"next_obs{i}"] for i in range(self.n_agents)]
+        action = torch.stack([batch[f"act{i}"].long() for i in range(self.n_agents)])
+        rewards = batch["rew"].view(-1, 1)
+        done = batch["done"]
+
+        with torch.no_grad():
+            q_tp1_values = torch.stack(self.critic(nobs))
+            q_next_states = torch.stack(self.target(nobs))
+        all_q_states = torch.stack(self.critic(obs))
+
+        _, a_prime = q_tp1_values.max(-1)
+
+        target_next_states = self.target_mixer(
+            q_next_states.gather(2, a_prime.unsqueeze(-1)), torch.concat(nobs, dim=-1)
+        ).detach()
+
+        target_states = rewards + self.gamma * target_next_states * (1 - done)
+
+        if self.standarize_returns:
+            self.ret_ms.update(target_states)
+            target_states = (target_states - self.ret_ms.mean.view(-1, 1)) / torch.sqrt(
+                self.ret_ms.var.view(-1, 1)
+            )
+
+        q_states = self.mixer(all_q_states.gather(2, action), torch.concat(obs, dim=-1))
+
+        loss = torch.nn.functional.mse_loss(q_states, target_states)
+
+        if self.grad_clip:
+            torch.nn.utils.clip_grad_norm_(self.critic.parameters(), self.grad_clip)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        self.update_from_target()
+
+    def soft_update(self, t):
+        super().soft_update(t)
+        try:
+            source, target = self.mixer, self.target_mixer
+        except AttributeError: # fix for when qmix has not initialised a mixer yet
+            return
+        for target_param, source_param in zip(target.parameters(), source.parameters()):
+            target_param.data.copy_((1 - t) * target_param.data + t * source_param.data)
