d5fc9034405737a7d9fa30cd9fe0473bbaf4b264
Lukas Schaefer <luki.schaefer96@gmail.com>
2023-10-20 10:38:32 +0100

    fix search script - hypergroup config argument doesn't exist --> need '+' to add to hydra - replace subprocess call to main.py with call to run.py (main.py   doesn't exist)

--- fastmarl/search.py
+++ fastmarl/search.py
@@ -20,7 +20,7 @@ def _flatten_lists(object):
 
 def _seed_and_shuffle(configs, shuffle, seeds):
 
-    configs = [[f"hypergroup=hp_grp_{i}"] + c for i, c in enumerate(configs)]
+    configs = [[f"+hypergroup=hp_grp_{i}"] + c for i, c in enumerate(configs)]
     configs = list(product(configs, [f"seed={i}" for i in range(seeds)]))
     configs = [list(_flatten_lists(c)) for c in configs]
 
@@ -103,7 +103,7 @@ def run(ctx, config, shuffle, seeds):
 )
 @click.pass_obj
 def locally(combos, cpus):
-    configs = ["python main.py " + "-m " + " ".join([c for c in combo]) for combo in combos]
+    configs = ["python run.py " + "-m " + " ".join([c for c in combo]) for combo in combos]
 
     click.confirm(
         f"There are {click.style(str(len(combos)), fg='red')} combinations of configurations. Up to {cpus} will run in parallel. Continue?",
